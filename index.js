5a7a5552a5b131a5d4c6e6b0412c93e9660707fa
Cory <corystephens012@gmail.com>
2021-07-19 13:50:27 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    let blocks = Math.abs(street - 42)
+    return blocks;
+}
+
+function distanceFromHqInFeet(blocks){
+    let feet = Math.abs(blocks - 42) * 264
+    return feet;
+}
+
+function distanceTravelledInFeet(start, end){
+    let distance = Math.abs(start - end) * 264
+    return distance;
+}
+
+function calculatesFarePrice(start, end){
+    let distance = Math.abs(start - end) * 264
+    if (distance < 400){
+        return 0;
+    } 
+    else if (distance > 400 && distance < 2000){
+        return (distance - 400) * .02
+    }
+    else if (distance > 2000 && distance < 2500){
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
